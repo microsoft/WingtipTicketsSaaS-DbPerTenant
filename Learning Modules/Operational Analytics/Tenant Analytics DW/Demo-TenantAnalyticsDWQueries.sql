6d9725996ecf0a0ae4d214663b78d15a82fe40ef
Debra Dove - MSFT <ddove@users.noreply.github.com>
2017-05-09 00:44:44 -0700

    typo in Tickets

--- Learning Modules/Operational Analytics/Tenant Analytics DW/Demo-TenantAnalyticsDWQueries.sql
+++ Learning Modules/Operational Analytics/Tenant Analytics DW/Demo-TenantAnalyticsDWQueries.sql
@@ -5,7 +5,7 @@
 -- Get a summary of all ticket sales from all tenants
 SELECT  VenueName, 
 		CONVERT(VARCHAR(30), SUM(PurchaseTotal), 1) AS TotalSales
-FROM    dbo.AllTickestPurchasesfromAllTenants 
+FROM    dbo.AllTicketsPurchasesfromAllTenants 
 GROUP   BY VenueName  
 ORDER   BY SUM(PurchaseTotal) DESC;  
 GO
