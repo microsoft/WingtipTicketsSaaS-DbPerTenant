94096ef2e3865e66fa3984d5a86e32005b43cf29
Bill Gibson <bill.gibson@microsoft.com>
2017-06-27 10:31:31 -0700

    Modifying UserConfig to simplify user update and enforce lower case convert on user value

--- Learning Modules/UserConfig.psm1
+++ Learning Modules/UserConfig.psm1
@@ -1,24 +1,27 @@
 ﻿<#
 .SYNOPSIS
-    Returns the User name and resource group name used during the WTP application deployment.  
+    Returns the User name and resource group name used during the Wingtip SaaS application deployment.  
     The values defined here are referenced by the learning module scripts.
 #>
+
+
+## Update the two variables below to the values used when the Wingtip SaaS app was deployed.   
+
+$ResourceGroupName = "<resourcegroup>" # the resource group used when the Wingtip SaaS application was deployed. CASE SENSITIVE
+$User              = "<user>"         # the User value entered when the Wingtip SaaS application was deployed
+
+##  DO NOT CHANGE VALUES BELOW HERE -------------------------------------------------------------
 
 function Get-UserConfig {
 
     $userConfig = @{`
-        ResourceGroupName = "<resourcegroup>"     # the resource group used when the WTP application was deployed. CASE SENSITIVE
-        Name =              $("<user>".ToLower()) # the User name entered when the WTP application was deployed  
+        ResourceGroupName = $ResourceGroupName     
+        Name =              $User.ToLower()   
     }
-
-
-
-    ##  DO NOT CHANGE VALUES BELOW HERE -------------------------------------------------------------
-
    
     if ($userConfig.ResourceGroupName -eq "<resourcegroup>" -or $userConfig.Name -eq "<user>")
     {
-        throw '$userConfig.ResourceGroupName and $userConfig.Name are not set.  Modify both values in UserConfig.psm1 and try again.'
+        throw 'UserConfig is not set.  Modify $ResourceGroupName and $User in UserConfig.psm1 and try again.'
     }
 
     return $userConfig
