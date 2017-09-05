eb1df44bb302d7eccc7abb1285f1dccd355a5cad
Bill Gibson <bill.gibson@microsoft.com>
2017-09-05 06:49:04 -0700

    Modify multi-tenant version of sp_DeleteEvent to require VenueId

--- WTP Application Project Files/WTP Application Project Files/WingtipSaaSDatabase/WingtipMultiTenantDB/dbo/StoredProcedures/sp_DeleteEvent.sql
+++ WTP Application Project Files/WTP Application Project Files/WingtipSaaSDatabase/WingtipMultiTenantDB/dbo/StoredProcedures/sp_DeleteEvent.sql
@@ -1,22 +1,23 @@
 ﻿CREATE PROCEDURE [dbo].[sp_DeleteEvent]
-	@EventId int
+	@VenueId int,
+    @EventId int
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-    DECLARE @Tickets int = (SELECT Count(*) FROM dbo.Tickets WHERE EventId = @EventId)
+    DECLARE @Tickets int = (SELECT Count(*) FROM dbo.Tickets WHERE VenueId = @VenueId AND EventId = @EventId)
 
     IF @Tickets > 0
     BEGIN
-        RAISERROR ('Error. Cannot delete events for which tickets have been purchased.', 11, 1)
+        RAISERROR ('Error. Cannot delete event for which tickets have been purchased.', 11, 1)
         RETURN 1
     END
 
     DELETE FROM dbo.[EventSections]
-    WHERE EventId = @EventId
+    WHERE VenueId = @VenueId AND EventId = @EventId
 
     DELETE FROM dbo.[Events]
-    WHERE EventId = @EventId
+    WHERE VenueId = @VenueId AND EventId = @EventId
 
     RETURN 0
 END
