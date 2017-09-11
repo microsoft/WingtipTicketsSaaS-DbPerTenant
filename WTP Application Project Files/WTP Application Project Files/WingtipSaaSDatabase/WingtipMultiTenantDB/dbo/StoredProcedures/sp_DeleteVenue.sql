1281a9cee212872b23910e32412e4f863f1bf9a7
Bill Gibson <bill.gibson@microsoft.com>
2017-09-11 09:31:52 -0700

    Add sp_DeleteVenue

---
+++ WTP Application Project Files/WTP Application Project Files/WingtipSaaSDatabase/WingtipMultiTenantDB/dbo/StoredProcedures/sp_DeleteVenue.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[sp_DeleteVenue]
+    @VenueId int = 0
+AS
+    IF @VenueId IS NULL
+    BEGIN
+        RAISERROR ('Error. @VenueId must be specified', 11, 1)
+        RETURN 1
+    END
+        
+    DELETE [dbo].[Tickets] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[TicketPurchases] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[Customers] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[EventSections] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[Sections] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[Events] WHERE VenueId = @VenueId
+    
+    DELETE [dbo].[Venues] WHERE VenueId = @VenueId
+
+RETURN 0
