66eb6712f435dda8cfd2d3ff52e7c5cade9a3b3b
Bill Gibson <bill.gibson@microsoft.com>
2017-10-20 11:28:52 -0700

    Removing multitenant database schema (will be available in a  repo dedicated to a multi-tenant version of the app)

--- WTP Application Project Files/WTP Application Project Files/WingtipSaaSDatabase/WingtipMultiTenantDB/dbo/Views/EventsWithNoTickets.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE VIEW [dbo].[EventsWithNoTickets]
-    AS SELECT VenueId, EventId, EventName, Subtitle, Date from dbo.Events as e
-    WHERE (SELECT Count(*) FROM dbo.Tickets AS t WHERE t.EventId=e.EventId and t.VenueId = e.VenueId) = 0
